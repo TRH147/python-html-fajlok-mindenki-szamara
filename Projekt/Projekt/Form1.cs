24342dab2167b63ef5d5fd81dd5ac48ce1fa2336
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-04-03 09:02:07 +0200

    Add files via upload
    Renáta glasc 04.03 Projekt HSZD

--- Projekt/Projekt/Form1.cs
+++ Projekt/Projekt/Form1.cs
@@ -15,9 +15,71 @@ namespace Projekt
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Items.Add("Jinx");
+            comboBox1.Items.Add("Aatrox");
+            comboBox1.Items.Add("Aphelios");
+
+            comboBox1.SelectedIndexChanged+=comboBox1_SelectedIndexChanged;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           string selectedForm=  comboBox1.SelectedItem.ToString();
+            Form newForm = null;
+
+            foreach (Form openForm in Application.OpenForms)
+            {
+                if (openForm.Text == selectedForm)
+                {
+                    openForm.Focus();
+                    return;
+
+                }
+            }
+            switch(selectedForm) 
+            {
+                case "Jinx":
+                    newForm = new Jinx();
+                    break;
+
+                case "Aatrox":
+                    newForm=new Aatrox();
+                    break;
+
+                case "Aphelios":
+                    newForm = new Form1();
+                    break;
+            }
+            if (newForm!=null)
+            {
+                newForm.Show();
+                this.Hide();
+
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
